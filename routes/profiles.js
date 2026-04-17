76838ebebaef6bf5e0165587d456b068eca8149e
Olamide Akinwande <wazirolamide22@gmail.com>
2026-04-17 23:52:39 +0100

    Replace uuidv7 with uuidv4 in profiles.js

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { v7: uuidv7 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const Profile = require('../models/Profile');
 
 const getAgeGroup = (age) => {

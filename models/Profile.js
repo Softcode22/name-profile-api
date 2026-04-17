794d34ee03de5146a3a22e01db69cdf97baee5be
Olamide Akinwande <wazirolamide22@gmail.com>
2026-04-17 22:26:51 +0100

    Create Profile model with schema definition

---
+++ models/Profile.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+  id: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
+  gender: { type: String },
+  gender_probability: { type: Number },
+  sample_size: { type: Number },
+  age: { type: Number },
+  age_group: { type: String },
+  country_id: { type: String },
+  country_probability: { type: Number },
+  created_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Profile', profileSchema);

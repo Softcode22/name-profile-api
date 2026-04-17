13cb2153de7b0a8832acd0cbc4bb409d5671ea83
Olamide Akinwande <wazirolamide22@gmail.com>
2026-04-17 22:42:09 +0100

    Modify server start condition based on NODE_ENV

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ connectDB();
 app.use('/api/profiles', profileRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
